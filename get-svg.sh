f4c69ea68c2eaeef545abab219cbe581ce18a763
Danijel-James Wynyard <danijel@repasscloud.com>
2025-09-06 17:12:22 +1000

    2025-09-06_17-12-22

---
+++ get-svg.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#curl -o bi-bus-front.svg https://icons.getbootstrap.com/assets/icons/bus-front.svg
+#./svg2css.sh bi-bus-front.svg bi-bus-front-nav-menu
+
+#curl -o bi-taxi-front.svg https://icons.getbootstrap.com/assets/icons/taxi-front.svg
+# ./svg2css.sh bi-taxi-front.svg bi-taxi-front-nav-menu
+
+#curl -o bi-train-front.svg https://icons.getbootstrap.com/assets/icons/train-front.svg
+# ./svg2css.sh bi-train-front.svg bi-train-front-nav-menu
+
+#curl -o bi-car-front.svg https://icons.getbootstrap.com/assets/icons/car-front.svg
+# ./svg2css.sh bi-car-front.svg bi-car-front-nav-menu
+
+#curl -o bi-simcard.svg https://icons.getbootstrap.com/assets/icons/sim.svg
+# ./svg2css.sh bi-simcard.svg bi-simcard-nav-menu
+
