2d45855223bb3e9fb162fe7867806280587f6de5
Danijel-James Wynyard <danijel@repasscloud.com>
2025-09-10 01:41:19 +1000

    working before change calendar again

--- BlazorLoginDemo.Web/Program.cs
+++ BlazorLoginDemo.Web/Program.cs
@@ -1,5 +1,7 @@
 using System.IO;
 using System.Globalization;
+using System.Collections.Specialized;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -17,7 +19,6 @@ using BlazorLoginDemo.Web.Security;   // SeedData
 using BlazorLoginDemo.Shared.Auth;
 using BlazorLoginDemo.Shared.Security;
 using BlazorLoginDemo.Shared.Services;
-using System.Collections.Specialized;
 
 using BlazorLoginDemo.Shared.Logging;
 using Serilog;
@@ -189,8 +190,12 @@ public class Program
 
         // Email Sender (MailerSend)
         builder.Services.Configure<MailerSendOptions>(builder.Configuration.GetSection("MailerSend"));
+
+        // Http
         builder.Services.AddHttpClient();
         builder.Services.AddAvaApiHttpClient(builder.Configuration);
+
+        // Services
         builder.Services.AddAvaClientServices();
         builder.Services.AddAvaFinanceServices();
         builder.Services.AddAvaPolicyServices();
