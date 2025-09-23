1c71cae0f12515e5d4ff83e7e5f8adb70803bde9
Danijel-James Wynyard <danijel@repasscloud.com>
2025-09-23 17:55:49 +1000

    replace legacy component

---
+++ BlazorLoginDemo.Shared/Models/Static/FareType.cs
@@ -0,0 +1,13 @@
+namespace BlazorLoginDemo.Shared.Models.Static;
+
+public static class FareType
+{
+
+    public static readonly (string code, string label)[] BookingClassOptions =
+    [
+        ("ECONOMY", "Economy"),
+        ("PREMIUM_ECONOMY", "Premium Economy"),
+        ("BUSINESS", "Business"),
+        ("FIRST", "First Class")
+    ];
+}
