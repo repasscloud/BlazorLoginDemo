e246ab479160d6e7ccc4fb132372e14165750f30
Danijel-James Wynyard <danijel@repasscloud.com>
2025-09-23 17:55:59 +1000

    update legacy component

--- BlazorLoginDemo.Shared/Models/Static/CabinClassCoverages.cs
+++ BlazorLoginDemo.Shared/Models/Static/CabinClassCoverages.cs
@@ -2,10 +2,10 @@ namespace BlazorLoginDemo.Shared.Models.Static;
 
 public static class CabinClassCoverages
 {
-    public static Dictionary<string, string> CabinClassCoverageOptions { get; set; } = new Dictionary<string, string>()
-    {
-        { "Most segments", "MOST_SEGMENTS" },
-        { "At least one segment", "AT_LEAST_ONE_SEGMENT" },
-        { "All segments", "ALL_SEGMENTS" }
-    };
+    public static readonly (string code, string label)[] CabinClassCoverageOptions =
+    [
+        ("MOST_SEGMENTS", "Most Segments"),
+        ("AT_LEAST_ONE_SEGMENT", "Min 1 Segment"),
+        ("ALL_SEGMENTS", "All Segments"),
+    ];
 }
