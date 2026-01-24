ecc6d778acf332e1a6ff988aaf512006b683ee48
Danijel-James Wynyard <danijel@repasscloud.com>
2026-01-24 12:11:22 +1100

    feat: add flexible enum converters and update TravelQuoteFlightUIResultPatchDto to use them fixes #89

--- Cinturon360.Shared/Models/DTOs/TravelQuoteFlightUIResultPatchDto.cs
+++ Cinturon360.Shared/Models/DTOs/TravelQuoteFlightUIResultPatchDto.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Cinturon360.Shared.Helpers;
 using Cinturon360.Shared.Models.Search;
 using Cinturon360.Shared.Models.Static.Travel;
 
@@ -9,6 +11,7 @@ public sealed class TravelQuoteFlightUIResultPatchDto
     public string Id { get; set; } = default!;
 
     // Customer Query Data
+    [JsonConverter(typeof(FlexibleEnumConverter<TripType>))]
     public TripType? TripType { get; set; }
     [MaxLength(3)] public string? OriginIataCode { get; set; }
     [MaxLength(3)] public string? DestinationIataCode { get; set; }
@@ -18,9 +21,15 @@ public sealed class TravelQuoteFlightUIResultPatchDto
     [MaxLength(8)] public string? DepartLatestTime { get; set; }
     [MaxLength(8)] public string? ReturnEarliestTime { get; set; }
     [MaxLength(8)] public string? ReturnLatestTime { get; set; }
+    
+    [JsonConverter(typeof(FlexibleEnumConverter<CabinClass>))]
     public CabinClass? CabinClass { get; set; }
+
+    [JsonConverter(typeof(FlexibleEnumConverter<CabinClass>))]
     public CabinClass? MaxCabinClass { get; set; }
     public string[] SelectedAirlines { get; set; } = Array.Empty<string>();
+
+    [JsonConverter(typeof(FlexibleEnumListConverter<AirlineAlliance>))]
     public List<AirlineAlliance>? Alliances { get; set; }
     public Guid Tid { get; set; } // transaction id
     public string? Uid { get; set; } // user id
